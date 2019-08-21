081ac5696ffd1265e99707eb94acaa78465915a0
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2019-08-21 09:11:47 -0400

    disp: msm: sde: add support for hardware based rounded corner
    Add support for hardware based rounded corner part of
    color processing framework.
    
    Change-Id: Ie05c5ecba6bd0323d1c8fd6949e12cec1f56b477
    Signed-off-by: Anjaneya Prasad Musunuri <aprasad@codeaurora.org>

--- techpack/display/msm/sde/sde_hw_mdss.h
+++ techpack/display/msm/sde/sde_hw_mdss.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2019, 2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _SDE_HW_MDSS_H
@@ -172,6 +172,7 @@ enum sde_stage {
 	SDE_STAGE_11,
 	SDE_STAGE_MAX
 };
+
 enum sde_dspp {
 	DSPP_0 = 1,
 	DSPP_1,
@@ -184,6 +185,12 @@ enum sde_ltm {
 	LTM_0 = DSPP_0,
 	LTM_1,
 	LTM_MAX
+};
+
+enum sde_rc {
+	RC_0 = DSPP_0,
+	RC_1,
+	RC_MAX
 };
 
 enum sde_ds {
