9dacc30dcc0d8e3ad1d0275de58cd34ad8ef3300
Sven Peter <sven@svenpeter.dev>
2021-06-07 08:17:51 +0200

    BACKPORT: usb: dwc3: support 64 bit DMA in platform driver
    Currently, the dwc3 platform driver does not explicitly ask for
    a DMA mask. This makes it fall back to the default 32-bit mask which
    breaks the driver on systems that only have RAM starting above the
    first 4G like the Apple M1 SoC.
    
    Fix this by calling dma_set_mask_and_coherent with a 64bit mask.
    
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sven Peter <sven@svenpeter.dev>
    Link: https://lore.kernel.org/r/20210607061751.89752-1-sven@svenpeter.dev
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 45d39448b4d0260743f25d88fd929451ec8296f2)
    Change-Id: Ib104ab043afd1b89c885c5631e437f9abea87185

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1536,6 +1536,10 @@ static int dwc3_probe(struct platform_device *pdev)
 
 	dwc3_get_properties(dwc);
 
+	ret = dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64));
+	if (ret)
+		return ret;
+
 	dwc->reset = devm_reset_control_get_optional_shared(dev, NULL);
 	if (IS_ERR(dwc->reset))
 		goto skip_clk_reset;
