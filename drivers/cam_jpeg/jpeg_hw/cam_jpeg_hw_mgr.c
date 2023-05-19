1e66a744f9ab2a0a9ac27e3b2f944fe3498a1a85
Shivakumar Malke <quic_smalke@quicinc.com>
2023-05-19 11:52:42 +0530

    msm: camera: mem_mgr: Add refcount to track in use buffers
    The function cam_mem_mgr_release can unmap the buffers when in use.
    
    This change prevents unmapping the buffers when in use.
    
    CRs-Fixed: 3489559
    Change-Id: I2e72e795d39ac15abfa56c19043c419a03686966
    Signed-off-by: Shivakumar Malke <quic_smalke@quicinc.com>
    Signed-off-by: Gaurav Jindal <quic_gjindal@quicinc.com>

--- drivers/cam_jpeg/jpeg_hw/cam_jpeg_hw_mgr.c
+++ drivers/cam_jpeg/jpeg_hw/cam_jpeg_hw_mgr.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/uaccess.h>
@@ -282,6 +282,8 @@ static int cam_jpeg_insert_cdm_change_base(
 	if (config_args->hw_update_entries[CAM_JPEG_CHBASE].offset >=
 		ch_base_len) {
 		CAM_ERR(CAM_JPEG, "Not enough buf");
+		cam_mem_put_cpu_buf(
+			config_args->hw_update_entries[CAM_JPEG_CHBASE].handle);
 		return -EINVAL;
 	}
 	CAM_DBG(CAM_JPEG, "iova %pK len %zu offset %d",
@@ -311,6 +313,9 @@ static int cam_jpeg_insert_cdm_change_base(
 	*ch_base_iova_addr = 0;
 	ch_base_iova_addr += size;
 	*ch_base_iova_addr = 0;
+
+	cam_mem_put_cpu_buf(
+		config_args->hw_update_entries[CAM_JPEG_CHBASE].handle);
 
 	return rc;
 }
@@ -1595,6 +1600,7 @@ hw_dump:
 		CAM_WARN(CAM_JPEG, "dump offset overshoot len %zu offset %zu",
 			jpeg_dump_args.buf_len, dump_args->offset);
 		mutex_unlock(&hw_mgr->hw_mgr_mutex);
+		cam_mem_put_cpu_buf(dump_args->buf_handle);
 		return -ENOSPC;
 	}
 
@@ -1605,6 +1611,7 @@ hw_dump:
 		CAM_WARN(CAM_JPEG, "dump buffer exhaust remain %zu min %u",
 			remain_len, min_len);
 		mutex_unlock(&hw_mgr->hw_mgr_mutex);
+		cam_mem_put_cpu_buf(dump_args->buf_handle);
 		return -ENOSPC;
 	}
 
@@ -1637,6 +1644,7 @@ hw_dump:
 	CAM_DBG(CAM_JPEG, "Offset before %u after %u",
 		dump_args->offset, jpeg_dump_args.offset);
 	dump_args->offset = jpeg_dump_args.offset;
+	cam_mem_put_cpu_buf(dump_args->buf_handle);
 	return rc;
 }
 
