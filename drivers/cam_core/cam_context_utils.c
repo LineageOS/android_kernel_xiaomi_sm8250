f28fbd16454a0112c9e7028393c7567d2435620c
Nirmal Abraham <quic_c_nabrah@quicinc.com>
2023-07-11 16:49:51 +0530

    msm: camera: mem_mgr: release buffers after usage
    Call cam_mem_put_cpu_buf corresponding to
    cam_mem_get_cpu_buf calls to make sure ref_cnt
    is balanced and buffer is freed when all
    clients are done with the buffer usage.
    
    CRs-Fixed: 3547081
    Change-Id: I9414829d6f17c368f2718fe05dbe25c71b31e674
    Signed-off-by: Nirmal Abraham <quic_c_nabrah@quicinc.com>

--- drivers/cam_core/cam_context_utils.c
+++ drivers/cam_core/cam_context_utils.c
@@ -301,6 +301,7 @@ int32_t cam_context_config_dev_to_hw(
 		(cmd->offset >= (len - sizeof(struct cam_packet)))) {
 		CAM_ERR(CAM_CTXT, "Not enough buf, len : %zu offset = %llu",
 			len, cmd->offset);
+		cam_mem_put_cpu_buf((int32_t) cmd->packet_handle);
 		return -EINVAL;
 
 	}
@@ -384,6 +385,7 @@ int32_t cam_context_prepare_dev_to_hw(struct cam_context *ctx,
 	if ((len < sizeof(struct cam_packet)) ||
 		(cmd->offset >= (len - sizeof(struct cam_packet)))) {
 		CAM_ERR(CAM_CTXT, "Not enough buf");
+		cam_mem_put_cpu_buf((int32_t) cmd->packet_handle);
 		return -EINVAL;
 
 	}
@@ -502,7 +504,7 @@ int32_t cam_context_prepare_dev_to_hw(struct cam_context *ctx,
 				req->in_map_entries[j].sync_id, rc);
 		}
 	}
-
+	cam_mem_put_cpu_buf((int32_t) cmd->packet_handle);
 	return rc;
 put_ref:
 	for (--i; i >= 0; i--) {
@@ -516,6 +518,7 @@ free_req:
 	req->ctx = NULL;
 	spin_unlock(&ctx->lock);
 
+	cam_mem_put_cpu_buf((int32_t) cmd->packet_handle);
 	return rc;
 }
 
