4c71b0dc467c0de609ea4ce682d5f37f877247b5
luka177 <lukapanio@gmail.com>
2023-10-15 23:01:07 +0200

    drivers: power: supply: ti: Import bq2597x charger pipa driver
    Change-Id: I3d5e6d00c94f1f10f4fce83b96314c02fde8df36

--- drivers/power/supply/ti/cp_qc30.h
+++ drivers/power/supply/ti/cp_qc30.h
@@ -101,12 +101,14 @@ enum hvdcp3_type {
 /* jeita related */
 #define JEITA_WARM_THR 450
 #define JEITA_COOL_NOT_ALLOW_CP_THR 100
+#define JEITA_COOL_NOT_ALLOW_CP_THR_PIPA 100
 /*
  * add hysteresis for warm threshold to avoid flash
  * charge and normal charge switch frequently at
  * the warm threshold
  */
 #define JEITA_HYSTERESIS 20
+#define JEITA_HYSTERESIS_PIPA 21
 #define JEITA_HYSTERESIS_DAGU 21
 
 #define HIGH_CAPACITY_TRH 85
