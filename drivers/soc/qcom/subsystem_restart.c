d6685d740d0bf71a076c3c600236b9bebd401d6a
mrsrimar22 <mrsrimar22@gmail.com>
2024-02-11 22:36:30 +0800

    qcom: ssr: Force RELATED restart_level for modem
    modem runs intensively when device is online,
    using SYSTEM restart_level on modem when device resume
    forces device to do hot_reboot with kernel_panic
    
    use RELATED restart_level, so that modem gets a chance
    to restart itself without relying on SSR workaround condition
    
    Change-Id: I274d99577c1aba96acc7dd3d7fdf2f13174d1be2

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1236,8 +1236,14 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return -EBUSY;
 	}
 
-	pr_info("Restart sequence requested for %s, restart_level = %s.\n",
-		name, restart_levels[dev->restart_level]);
+	if (!strcmp(name, "modem")) {
+		dev->restart_level = RESET_SUBSYS_COUPLED;
+		pr_warn("Restart sequence requested for %s, force restart_level to = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	} else {
+		pr_info("Restart sequence requested for %s, restart_level = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	}
 
 	if (disable_restart_work == DISABLE_SSR) {
 		pr_warn("subsys-restart: Ignoring restart request for %s\n",
