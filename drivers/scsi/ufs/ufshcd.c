919878589c6159d748276734cb6afe2ea6ca0b6d
stic-server-open <1138705738@qq.com>
2025-01-19 19:09:54 +0800

    drivers: ufs: Enable UFS write booster for xiaomi sm8250 platform
    This revert commit: 6c5839749 and 1271207a0 (only ufs_get_device_desc).
    * Xiaomi broke wb_config with the dagu-s-oss change, which causes
      ufshcd_wp_sup to always return false, and therefore ufshcd_wb_config
      will never be able to configure write booster.
    * After revert the above kernel changes, query ufs_flags
      ("/sys/devices/platform/soc/1d84000.ufshc/flags/wb_enable")
      has been changed to true.
    * Also partially revert d80d6657a, some xiaomi sm8250 platform use UFS3.0,
      for example, umi apollo thyme, These may not report the UFS_WB_TYPE,
      so use the UFS device feature to enable UFS_WB_FEATURE.
    
    change boot test (device: munch Redmi K40S):
    UFS vendor: SKhynix HN8T15BZGKX016
    
    Before:
    munch:/ # cat /sys/devices/platform/soc/1d84000.ufshc/flags/wb_enable
    false
    munch:/ # dmesg | grep "Write Booster Configured"
    1|munch:/ #
    
    Use Androbench test:
    Continuous Read: 1576.80 MB/s
    Continuous Write: 428.08 MB/s
    
    After:
    munch:/ # cat /sys/devices/platform/soc/1d84000.ufshc/flags/wb_enable
    true
    munch:/ # dmesg | grep "Write Booster Configured"
    ufshcd-qcom 1d84000.ufshc: ufshcd_wb_config: Write Booster Configured
    
    Use Androbench test:
    Continuous Read: 1702.49 MB/s
    Continuous Write: 750.23 MB/s
    
    Change-Id: I128439aff0656a3f4e5dbd793e181dea28273aa0
    Signed-off-by: stic-server-open <1138705738@qq.com>
    Signed-off-by: ZhaoYueNan <amktiao030215@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6878,10 +6878,8 @@ out:
 
 static bool ufshcd_wb_sup(struct ufs_hba *hba)
 {
-	return ((hba->dev_info.d_ext_ufs_feature_sup &
-		   UFS_DEV_WRITE_BOOSTER_SUP) &&
-		  (hba->dev_info.b_wb_buffer_type
-		   || hba->dev_info.wb_config_lun));
+	return !!(hba->dev_info.d_ext_ufs_feature_sup &
+		  UFS_DEV_WRITE_BOOSTER_SUP);
 }
 
 static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
@@ -8465,15 +8463,8 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 {
 	int err;
 	size_t buff_len;
-#ifdef CONFIG_MACH_XIAOMI_SM8250
 	u8 model_index;
-#else
-	u8 model_index, lun;
-#endif
 	u8 *desc_buf;
-#ifndef CONFIG_MACH_XIAOMI_SM8250
-	u32 d_lu_wb_buf_alloc;
-#endif
 
 	buff_len = max_t(size_t, hba->desc_size.dev_desc,
 			 QUERY_DESC_MAX_SIZE + 1);
@@ -8500,14 +8491,12 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 
 	model_index = desc_buf[DEVICE_DESC_PARAM_PRDCT_NAME];
 
-
-	/* Enable WB only for UFS-3.1 or UFS-2.2 OR if desc len >= 0x59 */
-	/*
-	if ((dev_desc->wspecversion >= 0x310) ||
+	/* Enable WB only for UFS-3.0 or UFS-2.2 OR if desc len >= 0x59 */
+	if ((dev_desc->wspecversion >= 0x300) ||
 	    (dev_desc->wspecversion == 0x220) ||
 	    (dev_desc->wmanufacturerid == UFS_VENDOR_TOSHIBA &&
 	     dev_desc->wspecversion >= 0x300 &&
-	     hba->desc_size.dev_desc >= 0x59)) {
+	     hba->desc_size.dev_desc >= 0x59))
 		hba->dev_info.d_ext_ufs_feature_sup =
 			desc_buf[DEVICE_DESC_PARAM_EXT_UFS_FEATURE_SUP]
 								<< 24 |
@@ -8516,35 +8505,7 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 			desc_buf[DEVICE_DESC_PARAM_EXT_UFS_FEATURE_SUP + 2]
 								<< 8 |
 			desc_buf[DEVICE_DESC_PARAM_EXT_UFS_FEATURE_SUP + 3];
-		hba->dev_info.b_wb_buffer_type =
-			desc_buf[DEVICE_DESC_PARAM_WB_TYPE];
 
-		if (hba->dev_info.b_wb_buffer_type)
-			goto skip_unit_desc;
-
-		hba->dev_info.wb_config_lun = false;
-		for (lun = 0; lun < UFS_UPIU_MAX_GENERAL_LUN; lun++) {
-			d_lu_wb_buf_alloc = 0;
-			err = ufshcd_read_unit_desc_param(hba,
-					lun,
-					UNIT_DESC_PARAM_WB_BUF_ALLOC_UNITS,
-					(u8 *)&d_lu_wb_buf_alloc,
-					sizeof(d_lu_wb_buf_alloc));
-
-			if (err)
-				break;
-
-			if (d_lu_wb_buf_alloc) {
-				hba->dev_info.wb_config_lun = true;
-				break;
-			}
-		}
-	}
-	*/
-
-#ifndef CONFIG_MACH_XIAOMI_SM8250
-skip_unit_desc:
-#endif
 	/* Zero-pad entire buffer for string termination. */
 	memset(desc_buf, 0, buff_len);
 
