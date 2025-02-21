53ed61cc3e711f441bb5cf5d73f1a2eb04219d83
lotan rm <lotanrm@googlemail.com>
2025-02-21 11:26:24 +0100

    sm8250-common: add _3LE formats to msm-dai-fe.c for munch
    fixes distortion problems on munch
    for more details, see https://gitlab.com/LineageOS/issues/android/-/issues/7470
    
    Change-Id: Idba02e864dfe954e967b92fcb65ff8a1ac3feb95

--- techpack/audio/asoc/msm-dai-fe.c
+++ techpack/audio/asoc/msm-dai-fe.c
@@ -854,8 +854,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Primary MI2S_TX Hostless Capture",
 			.aif_name = "PRI_MI2S_UL_HL",
 			.rates = SNDRV_PCM_RATE_8000_48000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE),
+#else
 			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE),
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min = 8000,
@@ -870,8 +876,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Primary MI2S_RX Hostless Playback",
 			.aif_name = "PRI_MI2S_DL_HL",
 			.rates = SNDRV_PCM_RATE_8000_384000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE),
+#else
 			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE),
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min =     8000,
@@ -886,8 +898,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Secondary MI2S_TX Hostless Capture",
 			.aif_name = "SEC_MI2S_UL_HL",
 			.rates = SNDRV_PCM_RATE_8000_48000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE),
+#else
 			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE),
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min = 8000,
@@ -902,8 +920,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Secondary MI2S_RX Hostless Playback",
 			.aif_name = "SEC_MI2S_DL_HL",
 			.rates = SNDRV_PCM_RATE_8000_384000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE,
+#else
 			.formats = SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE,
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min = 8000,
@@ -918,8 +942,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Tertiary MI2S_TX Hostless Capture",
 			.aif_name = "TERT_MI2S_UL_HL",
 			.rates = SNDRV_PCM_RATE_8000_48000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE),
+#else
 			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE),
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min = 8000,
@@ -934,8 +964,14 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 			.stream_name = "Tertiary MI2S_RX Hostless Playback",
 			.aif_name = "TERT_MI2S_DL_HL",
 			.rates = SNDRV_PCM_RATE_8000_384000,
+#if defined(CONFIG_MACH_XIAOMI_MUNCH)
+			.formats = SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE,
+#else
 			.formats = SNDRV_PCM_FMTBIT_S16_LE |
 				    SNDRV_PCM_FMTBIT_S24_LE,
+#endif
 			.channels_min = 1,
 			.channels_max = 2,
 			.rate_min =	8000,
