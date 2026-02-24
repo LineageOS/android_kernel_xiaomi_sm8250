2b4e3ba97ef61ad20c7a915ab98921e4224a73eb
Francescodario Cuzzocrea <bosconovic@gmail.com>
2026-02-24 19:34:57 +0100

    power: supply: bq2597x_charger_pipa: fix void to int pointer cast
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Change-Id: Iada79eea6f30090b1775843d766a1ae440401e5f

--- drivers/power/supply/ti/bq2597x_charger_pipa.c
+++ drivers/power/supply/ti/bq2597x_charger_pipa.c
@@ -1500,9 +1500,9 @@ static int bq2597x_get_dev_role(struct i2c_client *client)
 	}
 
 	dev_info(&client->dev, "%s: matched to %s, dev_role: %d.\n",
-			__func__, of_id->compatible, (int)of_id->data);
+			__func__, of_id->compatible, (uintptr_t)of_id->data);
 
-	return (int)of_id->data;
+	return (uintptr_t)of_id->data;
 }
 
 static int bq2597x_parse_dt(struct bq2597x *bq, struct device *dev)
